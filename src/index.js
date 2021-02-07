b07aca822fcc065443ae8f1e339c0dc4c34b5026
Mad Pillow <dmtr.schv@gmail.com>
2021-02-07 13:03:33 +0200

    init: solved

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
-module.exports = function reverse (n) {
-  
-}
+module.exports = function reverse(n) {
+    let m = 0;
+
+    console.log(n);
+    let str = Math.abs(n).toString().split("");
+    str = str.reverse();
+    m = parseInt(str.join(""));
+    console.log(m);
+    return m;
+};
